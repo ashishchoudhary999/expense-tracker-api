2be2243716d03a7b645603f569e0d474dc607d9b
ashishchoudhary999 <ashishchoudharyofficial0@gmail.com>
2026-05-26 23:33:40 +0530

    add create and get expense APIs

--- app/main.py
+++ app/main.py
@@ -1,7 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from app.database import engine, Base, get_db
+from app import models, schemas
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Expense Tracker API is running"}
+Base.metadata.create_all(bind=engine)
+
+@app.post("/expense")
+def create_expense(
+    expense: schemas.ExpenseCreate,
+    db: Session = Depends(get_db)
+):
+    new_expense = models.Expense(
+        title=expense.title,
+        amount=expense.amount,
+        category=expense.category
+    )
+
+    db.add(new_expense)
+    db.commit()
+    db.refresh(new_expense)
+
+    return new_expense
+
+@app.get("/expenses")
+def get_expenses(db: Session = Depends(get_db)):
+    expenses = db.query(models.Expense).all()
+
+    return expenses
