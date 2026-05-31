bdcd343ca75e40df189cb517a93ec5d8ad4d108d
ashishchoudhary999 <ashishchoudharyofficial0@gmail.com>
2026-06-01 00:41:08 +0530

    feat: add User model and schemas

--- app/models.py
+++ app/models.py
@@ -1,11 +1,18 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from .database import Base
 
 class Expense(Base):
     __tablename__ = "expenses"
 
     id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     title = Column(String, index=True)
     amount = Column(Float)
     category = Column(String)
     
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
--- app/schemas.py
+++ app/schemas.py
@@ -4,3 +4,18 @@ class ExpenseCreate(BaseModel):
     title: str
     amount: float
     category:str
+
+class UserCreate(BaseModel):
+    email: str
+    password: str
+
+class UserLogin(UserCreate):
+    pass
+
+class UserResponse(BaseModel):
+    id: int
+    email: str
+
+    class Config:
+        from_attributes = True
+
