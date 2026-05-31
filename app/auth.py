9f3b93365df2270bb32b315d300b26314f0682d6
ashishchoudhary999 <ashishchoudharyofficial0@gmail.com>
2026-06-01 02:30:23 +0530

    feat: add JWT token verification and get_current_user

--- app/auth.py
+++ app/auth.py
@@ -3,8 +3,16 @@ from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from jose import jwt
 from dotenv import load_dotenv
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from .database import get_db
+from .models import User
 
 load_dotenv()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/login")
+
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
@@ -23,4 +31,18 @@ def create_access_token(data: dict):
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     payload["exp"] = expire
     return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    
+def get_current_user(token: str = Depends(oauth2_scheme),
+                     db: Session = Depends(get_db)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    email = payload.get("sub")    
+    if email is None:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    user = db.query(User).filter(User.email == email).first()
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not found")
+    return user
     
