bd325de7f54362ca7726c1a1dda1c89d1a4734dd
ashishchoudhary999 <ashishchoudharyofficial0@gmail.com>
2026-06-01 02:09:09 +0530

    feat: add login endpoint with JWT token generation

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,14 @@
+import os
 from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import jwt
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -8,3 +18,9 @@ def hash_password(password):
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
+def create_access_token(data: dict):
+    payload = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    payload["exp"] = expire
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    
--- app/routes/user.py
+++ app/routes/user.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from ..database import get_db
 from ..models import User
-from ..schemas import UserCreate, UserResponse
-from ..auth import hash_password
+from ..auth import hash_password, verify_password, create_access_token
+from ..schemas import UserCreate, UserResponse, UserLogin
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -21,4 +21,14 @@ def register(user: UserCreate, db:Session = Depends(get_db)):
     db.refresh(new_user)
 
     return new_user
+
+@router.post("/login")
+def login(user: UserLogin, db:Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    if not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")    
+    token = create_access_token(data={"sub": db_user.email})    
+    return {"access_token": token, "token_type": "bearer"}
 
